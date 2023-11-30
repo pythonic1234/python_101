561cc6d541eff36d7f27b6918e01de09643dd2b8
pythonic1234 <152273153+pythonic1234@users.noreply.github.com>
2023-11-30 21:52:43 +0900

    Add files via upload

---
+++ Day02/calk.py
@@ -0,0 +1,48 @@
+연도 = input("태어난 연도를 입력해주세요")
+
+
+
+
+
+number1 = float(input("첫 번째 숫자를 입력하세요: "))
+number2 = float(input("두 번째 숫자를 입력하세요: "))
+나누기 = number1 // number2
+print(f"두 수의 나누기 결과는 {나누기}입니다")
+
+
+number1 = float(input("첫 번째 숫자를 입력하세요: "))
+number2 = float(input("두 번째 숫자를 입력하세요: "))
+나누기 = number1 / number2
+나머지 = number1 % number2
+
+print(f"나눗셈 결과: {나누기}")
+print(f"나머지 결과: {나머지}")
+
+
+number1 = float(input("첫 번째 숫자를 입력하세요: "))
+number2 = float(input("두 번째 숫자를 입력하세요: "))
+print(int(number1 - number2))
+difference = number1-number2
+
+print(f"두 숫자의 차이는 {difference}입니다")
+
+
+num1 = float(input("숫자1"))
+num2 = float(input("숫자2"))
+
+print(int(num1)/(num2))
+
+
+a = input("숫자1 입력")
+b = input("숫자2 입력")
+x = int(a) * int(b)
+
+print(f"두 수를 곱한 결과는 {x}입니다")
+
+
+
+
+
+
+
+
---
+++ Day02/data file.py
@@ -0,0 +1,39 @@
+#a = 1
+#b = 1.5
+# 해당 데이터의 타입을 알려줌
+# type()
+#print(type(a))
+
+#print(type("안"))
+
+# a = int("숫자 하나 입력")
+# b = int("숫자 두번째 입력")
+# print(type(a))
+# print(type(b))
+# add = a + b
+# print(f"두 수의 합은 {a+b}입니다")
+
+# a = int(input("숫자1을 입력해주세요"))
+# b = int(input("숫자2를 입력해주세요"))
+# print(f"두 수의 합은 {a+b}입니다")
+
+# num1 = int(3.14)
+# num2 = int(10)
+#
+# print(num1+num2)
+
+# int()
+# type()
+# print()
+# input()
+
+# a = int("10")
+# b = int("20")
+# print(f"{a+b} 입니다")
+
+
+
+
+
+
+
